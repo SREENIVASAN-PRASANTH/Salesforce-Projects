031e47af628a94048e257658fa7726ce42bd59ef
Prasanth <prasanthshot@gmail.com>
2025-07-22 18:43:23 +0530

    completed contacts and accounts management app

--- LWC Project1/force-app/main/default/lwc/accountContacts/accountContacts.js
+++ LWC Project1/force-app/main/default/lwc/accountContacts/accountContacts.js
@@ -1,11 +1,25 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
 import {MessageContext, subscribe, unsubscribe } from 'lightning/messageService';
 import AccountContactChannel from '@salesforce/messageChannel/AccountContact__c';
+import getAccountContacts from '@salesforce/apex/AccountClass.getAccountContacts';
+import LightningConfirm from 'lightning/confirm';
+// import { deleteRecord } from 'lightning/uiRecordApi';
+// import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { deleteRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class AccountContacts extends LightningElement {
-    currentId;
-    currentName;
-    title;
+    accountId;
+    accountName;
+    title = "Contacts";
+    contacts;
+    hasContacts;
+    isAccountSelected = false;
+    isAddContactClicked = false;
+    isEditContactClicked = false;
+    editableContactId;
+
+    @api recordId;
     subscription = null;
 
     @wire (MessageContext) messageContext;
@@ -22,12 +36,67 @@ export default class AccountContacts extends LightningElement {
         if(!this.subscription){
             this.subscription = subscribe(this.messageContext, AccountContactChannel,
                 (parameter) => {
-                    this.currentId = parameter.Id;
-                    this.currentName = parameter.Name;
+                    this.accountId = parameter.Id;
+                    this.accountName = parameter.Name;
                     this.title = parameter.Name + "'s Contacts"
+                    this.getContacts();
                 }
             )
         }
+    }
+
+    async getContacts(){
+        this.contacts = await getAccountContacts({accountId: this.accountId});
+        this.hasContacts = this.contacts.length > 0?true:false;
+        this.isAccountSelected = true;
+    }
+
+    handleAddContact(event){
+        this.isAddContactClicked = true;
+    }
+
+    handleAddContactCancel(event){
+        this.isAddContactClicked = false;
+    }
+
+    handleEditContact(event){
+        this.isEditContactClicked = true;
+        this.editableContactId = event.target.dataset.contactId
+    }
+
+    handleEditContactCancel(event){
+        this.isEditContactClicked = false;
+    }
+
+    handleSuccess(event){
+        this.isAddContactClicked = false;
+        this.isEditContactClicked = false;
+        this.getContacts();
+    }
+
+    async handleDelete(event){
+        this.editableContactId = event.target.dataset.contactId;
+        const result = await LightningConfirm.open({
+            message: 'Are you Sure to Delete?',
+            variant: 'headerless',
+            label: 'this is the aria-label value',
+            // setting theme would have no effect
+        });
+
+        if(result){
+            let deleteResult = await deleteRecord(this.editableContactId);
+            this.getContacts();
+            this.showToast();
+        }
+    }
+
+    showToast() {
+        const event = new ShowToastEvent({
+            title: 'Delete Contact',
+            message:
+                'Contact deleted Successfully.',
+        });
+        this.dispatchEvent(event);
     }
 
     handleUnsubscription(){
